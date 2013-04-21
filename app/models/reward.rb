19c96428209679bd69fe8358cb742509e16a10c4
Jason Shen <jasonyshen@gmail.com>
2013-04-21 11:57:01 -0700

    Change the reedem method in Reward model to use offset instead of simple find.by.id

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,7 +2,8 @@ class Reward < ActiveRecord::Base
   attr_accessible :description, :name, :size
 
   def self.redeem
-  	return Reward.find_by_id(rand(Reward.count)+1)
+  	offset = rand(Reward.count)
+  	return Reward.first(:offset => offset)
   end
 
 end
