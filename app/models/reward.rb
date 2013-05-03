87a0aaa89ab20fe2d38f2ee01f853509723e4bbd
Jason Shen <jasonyshen@gmail.com>
2013-05-02 18:16:39 -0700

    added google analytics, made a very basic method in reward model for logged out users to try it

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,4 +3,14 @@ class Reward < ActiveRecord::Base
   belongs_to :user
   validates :description, :length => { :maximum => 140 }
 
+
+  def self.test_redeem
+    reward_array = []
+    reward_array.push(Reward.new(name: "game", description: "Whip out your iPhone and spend 5 minutes playing Angry Birds Space", size: 3))
+    reward_array.push(Reward.new(name: "candy", description: "Break off a small piece of dark chocolate. Savor it.", size: 3))
+    reward_array.push(Reward.new(name: "dance", description: "Silent dance party to Gangnam Style. Go", size: 3))
+    random = rand(reward_array.size)-1
+    return reward_array[random]
+  end
+
 end
