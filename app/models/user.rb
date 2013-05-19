b21a4978be94f9d8e4d1eb515c7e04f7c5ad5027
Jason Shen <jasonyshen@gmail.com>
2013-05-18 19:26:59 -0700

    Create functionality so Size 1 rewards are much more frequent than Size 2, and 2 to 3. Added a small explanation in the form partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,44 @@ class User < ActiveRecord::Base
   end
 
   def redeem
-    offset = rand(rewards.count)
-    return rewards.first(:offset => offset)
+    num = rand(12)
+    case num
+    when 0
+      return self.random_reward(3).description rescue self.random_reward(2).description rescue self.random_reward(1).description
+    when 1..3
+      return  self.random_reward(2).description rescue self.random_reward(1).description
+    when 4..11
+      return self.random_reward(1).description
+    end
   end
+
+
+  # returns arrays with random reward of that specific size (1 = small, 2 = medium 3 = big)
+  def random_reward(reward_size)
+    @specific_size = []
+    self.rewards.all.each do |reward|
+      if reward.size == reward_size
+        @specific_size << reward
+      end
+    end
+    return @specific_size.sample
+  end
+
+  # returns array with 1 or zeros for each of the different sizes
+  def size_check
+    @size_check = [0,0,0]
+    self.rewards.each do |reward|
+      case reward.size
+      when 1
+        @size_check[0] = 1
+      when 2
+        @size_check[1] = 1
+      when 3
+        @size_check[2] = 1
+      end
+    end
+    return @size_check
+  end
+
 
 end
